bcca7c50638c450438e827d16c4200281c0f4326
ThankedDeer <eduardo.salinas12@hotmail.com>
2024-10-19 02:03:44 -0600

    initial base

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from './App';
 import './index.scss';
 import { ActorProvider, AgentProvider } from '@ic-reactor/react';
 import { idlFactory, canisterId } from './declarations/backend';
+import { NextUIProvider } from '@nextui-org/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AgentProvider withProcessEnv>
       <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
-        <App />
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
       </ActorProvider>
     </AgentProvider>
   </React.StrictMode>,
