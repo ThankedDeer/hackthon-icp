80767cc936561b3750ee7da9604751899fe658d2
ThankedDeer <eduardo.salinas12@hotmail.com>
2024-10-19 02:36:41 -0600

    nextui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <>hola</>;
+  return <>Header</>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,29 @@ import './index.scss';
 import { ActorProvider, AgentProvider } from '@ic-reactor/react';
 import { idlFactory, canisterId } from './declarations/backend';
 import { NextUIProvider } from '@nextui-org/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <div>Not Found</div>,
+    children: [
+      {
+        path: 'monitors',
+        element: <div>monitor</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AgentProvider withProcessEnv>
       <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
         <NextUIProvider>
-          <App />
+          <RouterProvider router={router} />
         </NextUIProvider>
       </ActorProvider>
     </AgentProvider>
